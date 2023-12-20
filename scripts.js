a00c22f9811fa4819829f129a24e46bc624ac79b
joaocorreiaprof <joaocorreiaprof@gmail.com>
2023-12-20 22:43:32 +0000

    Added functions: User can choose number of grids and when moving the mouse leaves a pen trail

--- scripts.js
+++ scripts.js
@@ -2,11 +2,24 @@
 function createGrid(rows, cols) {
   const container = document.getElementById("container");
 
+  //makes the refresh after click "Size Me"
+  container.innerHTML = "";
+
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       const cell = document.createElement("div");
       cell.className = "grid-cell";
       container.appendChild(cell);
+
+      // AddeventListener for mouse enter and mouse leave
+      cell.addEventListener(
+        "mouseenter",
+        () => (cell.style.background = "blue")
+      );
+      cell.addEventListener(
+        "mouseleave",
+        () => (cell.style.background = "blue")
+      );
     }
 
     // Add a line break after each row
@@ -15,5 +28,17 @@ function createGrid(rows, cols) {
   }
 }
 
-// Call the function to create a 16x16 grid
+// Call the function to create a 16x16 grid- Standard
 createGrid(16, 16);
+
+//trying to prompt
+
+const button = document.getElementById("numberOfGrids");
+button.onclick = chooseSize;
+
+function chooseSize() {
+  let size = prompt("What size do you wish for your game? MAX 50");
+  if (size != null && size <= 50) {
+    createGrid(size, size);
+  }
+}
